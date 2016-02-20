02c8e7e311ba7034c0b7446dfe97b23dedd0a7c4
Alex Silverstein <aslvrstn@gmail.com>
2016-02-20 13:42:29 -0500

    Solution to 1.2

--- Set1.hs
+++ Set1.hs
@@ -5,6 +5,15 @@ module Set1 where
 
 import MCPrelude
 
+-- 1.1
 fiveRands = take 5 (rands(mkSeed 1))
 
 rands s = fst(rand(s)) : rands(snd(rand(s)))
+
+
+-- 1.2
+randLetter s = (toLetter(fst(rand(s))), snd(rand(s)))
+
+randLetters s = fst(randLetter(s)) : randLetters(snd(randLetter(s)))
+
+randString3 = take 3 (randLetters(mkSeed 1))
